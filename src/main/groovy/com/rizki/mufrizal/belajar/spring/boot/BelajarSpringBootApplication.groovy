0845d0e00fff44cc663f75ccfb828a54906e7815
RizkiMufrizal <mufrizalrizki@gmail.com>
2017-01-13 22:51:09 +0700

    tambah main class

---
+++ src/main/groovy/com/rizki/mufrizal/belajar/spring/boot/BelajarSpringBootApplication.groovy
@@ -0,0 +1,12 @@
+package com.rizki.mufrizal.belajar.spring.boot
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class BelajarSpringBootApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run BelajarSpringBootApplication, args
+    }
+}
---
+++ src/test/groovy/com/rizki/mufrizal/belajar/spring/boot/BelajarSpringBootApplicationTests.groovy
@@ -0,0 +1,16 @@
+package com.rizki.mufrizal.belajar.spring.boot
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner)
+@SpringBootTest
+class BelajarSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
