6e5f6fede0823e1217780cd3eddb5b1a0fc8f16e
RizkiMufrizal <mufrizalrizki@gmail.com>
2017-01-13 22:49:59 +0700

    tambah enum jenis barang

---
+++ src/main/groovy/com/rizki/mufrizal/belajar/spring/boot/domain/JenisBarang.groovy
@@ -0,0 +1,16 @@
+package com.rizki.mufrizal.belajar.spring.boot.domain
+
+/**
+ *
+ * @Author Rizki Mufrizal <mufrizalrizki@gmail.com>
+ * @Web <https://RizkiMufrizal.github.com>
+ * @Since 12 January 2017
+ * @Time 10:15 PM
+ * @Project Belajar-Spring-Boot
+ * @Package com.rizki.mufrizal.belajar.spring.boot.domain
+ * @File JenisBarang
+ *
+ */
+enum JenisBarang {
+    gas, padat, cair
+}
